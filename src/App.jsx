96e4f4c5d485da03c7947c2e3dadad4833989022
sebastian lozano <sebaslozano708@gmail.com>
2024-10-02 21:57:06 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+
+
+export default function App() {
+  return (
+    <main className="w-full h-screen bg-[#FDE8E9]">
+      <Header />
+    </main>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Header() {
+  return (
+    <header className="w-full border-2 border-red-800 border-dashed flex justify-center py-4" >
+        <h1 className="font-bold text-4xl" >TODOLIST</h1>
+    </header>
+  )
+}
